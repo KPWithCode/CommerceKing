597fa8a1a33708ab98084f101d125d07ec2b1a36
Kyle Phillips <khillips375@yahoo.com>
2020-09-30 12:02:34 -0400

    Smart Contract for administering Payments

---
+++ contracts/Payments.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.6.2;
+
+// erc20 interface
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract Payments {
+    address public admin;
+    IERC20 public Dai;
+
+
+// event for Payment bein gDone
+    event PaymentDone(
+        address payer,
+        uint paymentId,
+        // dates are represented as integers in solidity
+        uint date
+    );
+
+    constructor(address adminAddress, address daiAddress) public {
+        admin = adminAddress;
+        dai = IERC20(daiAddress);
+    }
+
+    function pay(uint amount, uint paymentId) external {
+        // transferFrom has owner, recipient and amount
+        dai.transferFrom(msg.sender, admin, amount);
+        // emit event
+        emit PaymentDone(msg.sender, amount, paymentId, block.timestamp)
+    }
+}

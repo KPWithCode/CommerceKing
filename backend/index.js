4b145d58dc158cd36cfee60aaf5c6d956193f2d0
Kyle Phillips <khillips375@yahoo.com>
2020-10-04 02:29:58 -0400

    Able to run to port and connect to mongodb

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,28 @@ const Router = require('@koa/router');
 const cors = require('@koa/cors');
 const ethers = require('ethers');
 const PaymentProcessor = require('../build/contracts/Payments.json');
-const { Payment } = require('db.js');
+const { Payment } = require('./db.js');
+const mongoose = require('mongoose');
+
 
 const app = new Koa()
 const router = new Router()
 
-router.get('/api/getPaymentId')
+router.get('/api/getPaymentId/:itemId', async ctx => {
+    ctx.body = 'hello world'
+})
+
+app.use(cors())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+
+const DB = require('./config/keys').mongoURI
+
+//  Connect to Mongo
+mongoose.connect(DB,{useNewUrlParser: true})
+.then(() => console.log('Mongo DB connected 🚀🚀🚀'))
+.catch(err => console.log(err))
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Commerce commencing on port ${port} 🛍️🛍️🛍️`))

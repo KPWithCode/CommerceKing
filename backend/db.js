30e005e83cdf883924b0e3eb8d83e2933728937a
Kyle Phillips <khillips375@yahoo.com>
2020-10-04 02:29:14 -0400

    Schema skeleton

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-    'connection string',
-    {useNewUrlParser: true, useUnifiedTopology: true}
-);
-
 const paymentSchema = new mongoose.Schema({
     id: String,
     itemId: String,

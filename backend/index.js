909b24cd52b45890e081726e2115b342d11e654b
Kyle Phillips <khillips375@yahoo.com>
2020-10-05 05:06:33 -0400

    Routes and function for listening to events'

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,51 @@ const Router = require('@koa/router');
 const cors = require('@koa/cors');
 const ethers = require('ethers');
 const PaymentProcessor = require('../build/contracts/Payments.json');
-const { Payment } = require('./db.js');
+const { Payment } = require('./models/payModel');
 const mongoose = require('mongoose');
+// const paymentRouter = require('./routes/api/payments');
 
 
 const app = new Koa()
 const router = new Router()
 
-router.get('/api/getPaymentId/:itemId', async ctx => {
-    ctx.body = 'hello world'
-})
+const items = {
+    '1': {id: 1, url: 'http://UrlToDownloadItem1' },
+    '2': {id: 2, url: 'http://UrlToDownloadItem2' },
 
+}
+router.get('/api/getPaymentId/:itemId', async ctx => {
+    const paymentId = (Math.random() * 1000).toFixed(0);
+    await Payment.create({
+        id: paymentId,
+        itemId: ctx.params.itemId,
+        paid: false
+    })
+    ctx.body = {
+        paymentId
+    }
+})
+router.get('/api/getItemUrl/:paymentId', async ctx => {
+    const payment = await Payment.findOne({id: ctx.params.paymentId})
+    if (payment && payment.paid === true) {
+        ctx.body = {
+            url: items[payment.itemId].url
+        }
+    } else {
+        ctx.body = {
+            url:''
+        };
+    }
+});
 app.use(cors())
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 
-const DB = require('./config/keys').mongoURI
 
+// app.use('/payments', paymentRouter);
+
+const DB = require('./config/keys').mongoURI
 //  Connect to Mongo
 mongoose.connect(DB,{useNewUrlParser: true})
 .then(() => console.log('Mongo DB connected 🚀🚀🚀'))
@@ -28,3 +55,30 @@ mongoose.connect(DB,{useNewUrlParser: true})
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Commerce commencing on port ${port} 🛍️🛍️🛍️`))
+
+
+const listenToEvents = () => {
+    // 9545 is the default host when deploying smart contracts to ganache
+    const provider = new ethers.providers.JsonRpcProvider('http://localhost:9545');
+    const networkId = '5777';
+
+    const paymentProcessor = new ethers.Contract(
+        PaymentProcessor.networks[networkId].address,
+        PaymentProcessor.abi,
+        provider
+    )
+    paymentProcessor.on('PaymentDone', async (payer, amount, paymentId,date) => {
+        console.log(`
+        from ${payer}
+        amount ${amount}
+        paymentId ${paymentId}
+        date ${(new Date(date.toNumber() * 1000)).toLocaleString()}
+        `)
+        const payment = await Payment.findOne({id: paymentId});
+        if (payment) {
+            payment.paid = true;
+            await payment.save();
+        }
+    });
+}
+listenToEvents()

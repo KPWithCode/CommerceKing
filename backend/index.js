13ba6072087a37cc12dd8ce36d55be17089b85ca
Kyle Phillips <khillips375@yahoo.com>
2020-10-07 10:33:52 -0400

    removing router requests

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const Router = require('@koa/router');
 const cors = require('@koa/cors');
 const ethers = require('ethers');
 const PaymentProcessor = require('../build/contracts/Payments.json');
-const { Payment } = require('./models/payModel');
+// const { Payment } = require('./models/payModel');
 const mongoose = require('mongoose');
 // const paymentRouter = require('./routes/api/payments');
 
@@ -11,34 +11,34 @@ const mongoose = require('mongoose');
 const app = new Koa()
 const router = new Router()
 
-const items = {
-    '1': {id: 1, url: 'http://UrlToDownloadItem1' },
-    '2': {id: 2, url: 'http://UrlToDownloadItem2' },
+// const items = {
+//     '1': {id: 1, url: 'http://UrlToDownloadItem1' },
+//     '2': {id: 2, url: 'http://UrlToDownloadItem2' },
 
-}
-router.get('/api/getPaymentId/:itemId', async ctx => {
-    const paymentId = (Math.random() * 1000).toFixed(0);
-    await Payment.create({
-        id: paymentId,
-        itemId: ctx.params.itemId,
-        paid: false
-    })
-    ctx.body = {
-        paymentId
-    }
-})
-router.get('/api/getItemUrl/:paymentId', async ctx => {
-    const payment = await Payment.findOne({id: ctx.params.paymentId})
-    if (payment && payment.paid === true) {
-        ctx.body = {
-            url: items[payment.itemId].url
-        }
-    } else {
-        ctx.body = {
-            url:''
-        };
-    }
-});
+// }
+// router.get('/api/getPaymentId/:itemId', async ctx => {
+//     const paymentId = (Math.random() * 1000).toFixed(0);
+//     await Payment.create({
+//         id: paymentId,
+//         itemId: ctx.params.itemId,
+//         paid: false
+//     })
+//     ctx.body = {
+//         paymentId
+//     }
+// })
+// router.get('/api/getItemUrl/:paymentId', async ctx => {
+//     const payment = await Payment.findOne({id: ctx.params.paymentId})
+//     if (payment && payment.paid === true) {
+//         ctx.body = {
+//             url: items[payment.itemId].url
+//         }
+//     } else {
+//         ctx.body = {
+//             url:''
+//         };
+//     }
+// });
 app.use(cors())
 app.use(router.routes())
 app.use(router.allowedMethods())

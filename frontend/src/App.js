a1c8159377e13d28b2ebca63a01ee1bb5847238b
Kyle Phillips <khillips375@yahoo.com>
2020-10-09 23:54:19 -0400

    paymentprocessor and dai ready..logic for when metamask is not installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import getBlockchain from './eth';
 
 function App() {
+  const [paymentProcessor, setPaymentProcessor] = useState(undefined);
+  const [dai, setDai] = useState(undefined);
+
+  useEffect(async() => {
+    const init = async () => {
+      const { paymentProcessor,dai} = await getBlockchain();
+      setPaymentProcessor(paymentProcessor);
+      setDai(dai);
+    }
+    init();
+  })
+  // metamask not installed?
+  if (typeof window.ethereum === 'undefined') {
+    return (
+      <div className='container'>
+        <div className='col-sm-12'>
+          <h1> Welcome to KCommerce</h1>
+          <p>Install the latest version of metamask</p>
+        </div>
+      </div>
+    )
+  }
   return (
-    <div>
-      Hello World
-    </div>
+    <div className='container'>
+        <div className='col-sm-12'>
+          <h1> Welcome to KCommerce</h1>
+          {/* <Store paymentProcessor={paymentProcessor} dai={dai} /> */}
+        </div>
+      </div>
   );
 }
 
